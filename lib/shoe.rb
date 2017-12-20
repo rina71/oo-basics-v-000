f860543d297e6bc76622ba76b12ced7867af1787
Yurina Shin <yurinas@gmail.com>
2017-12-20 22:01:41 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,4 +4,9 @@ class Shoe
   def initialize(brand)
 @brand = brand
   end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    self.condition = "new"
+  end
 end# Make your shoe class here!
